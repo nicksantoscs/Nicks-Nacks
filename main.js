33eb2becfb2d738477e2df19a89024e52ad4ca6a
Nicholas Santos <55286153+nicksantoscs@users.noreply.github.com>
2020-03-21 22:53:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,47 @@
 //Nicholas Santos; March 21, 2020
 
-let requestURL = 'https://github.com/nicksantoscs/Weird-Items-Page/blob/master/mod4.json';
+let requestURL = 'https://nicksantoscs.github.io/Weird-Items-Page/mod4.json';
 
-//XHR Object allows the server to update without hitting the refresh button, grabs things from the server without refresh 
+//XML Object allows the server to update without hitting the refresh button, grabs things from the server without refresh 
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json'; 
 request.send();
 
 request.onload = function() {
-  let weirdItems = request.response;
-  console.log(weirdItems); 
-  itemInfo(weirdItems); 
+  let itemInfo = request.response;
+  console.log(itemInfo); 
+  weirdItems(itemInfo); 
 };
 
-function itemInfo(jsonObj) {
+function weirdItems(jsonObj) {
   
-  let itemInfo = jsonObj.itemInfo; 
+let weirdItems = jsonObj.weirdItems; 
   
-  for(let i = 0; i < itemInfo.length; i++) {
+for(let i = 0; i < weirdItems.length; i++) {
     
-//HTML elements for the web page 
-    let article = document.createElement('article');
-    let h2 = document.createElement('h2');
-    let img = document.createElement('img');
+//HTML elements for the web page
     let p1 = document.createElement('p');
     let p2 = document.createElement('p');
-    let ul = document.createElement('ul');
+    let h2 = document.createElement('h2');
+    let img = document.createElement('img');    
+    let article = document.createElement('article');
+    let section = document.querySelector('section');
 
-    let section = document.querySelector('section'); 
-    img.setAttribute('src', 'https://github.com/nicksantoscs/Weird-Items-Page/tree/master/assets' + itemInfo[i].image); 
-    img.setAttribute('alt', itemInfo[i].image); 
-    h2.textContent = itemInfo[i].name; 
-    p1.textContent = 'price' + itemInfo[i].price;
-    p2.textContent = 'details' + itemInfo[i].details;
-
-    let weirdItems = itemInfo[i].weirdItems; 
-    for (let x = 0; x < weirdItems.length; x++) {
-      let listItem = document.createElement('li'); 
-      listItem.textContent = weirdItems[x]; 
-      ul.appendChild(listItem); 
-    }
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(p1); 
-    article.appendChild(p2);
-    article.appendChild(ul); 
-    section.appendChild(article);
-
+//Set attribute for images
+    img.setAttribute('src', 'https://nicksantoscs.github.io/Weird-Items-Page/assets/' + weirdItems[i].image); 
+    img.setAttribute('alt', weirdItems[i].image);
+    
+//Text content
+    p1.textContent = 'Price: ' + weirdItems[i].price;
+    p2.textContent = 'Details: ' + weirdItems[i].details;
+    h2.textContent = weirdItems[i].name; 
+    
+//Append child
+article.appendChild(p1); 
+article.appendChild(p2);    
+article.appendChild(h2);
+article.appendChild(img);    
+section.appendChild(article);
   }
- 
 }
